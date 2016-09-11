4b2d62cf28d287f92e55d782aae2da13ccf20317
clovett <lovettchris@hotmail.com>
2016-09-10 19:30:08 -0700

    Initial checkin.

---
+++ Version.cs
@@ -0,0 +1,31 @@
+//
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+using System;
+using System.Reflection;
+
+[assembly: AssemblyTitle("SgmlReader")]
+[assembly: AssemblyDescription("Converts SGML to XML via XmlReader API")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright (c) 2002-2016, Microsoft Corporation; Copyright (c) 2007-2013, MindTouch")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+
+//
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.8.12")]
+[assembly: AssemblyFileVersion("1.8.12")]
