6cea004823f47529f0184d0bc50ae15712578945
clovett <lovettchris@hotmail.com>
2021-07-20 15:57:56 -0700

    add testing, signing and packaging steps.

--- BUILD.cmd
+++ BUILD.cmd
@@ -1,3 +1,4 @@
+@echo off
 
 @WHERE msbuild
 @IF %ERRORLEVEL% NEQ 0 (
@@ -14,7 +15,19 @@
 pwsh -f Common/fix_versions.ps1
 
 REM Restore NuGet packages:
-msbuild -t:restore SgmlReader.sln
+msbuild -t:restore SgmlReader.sln /verbosity:minimal
 
 REM Build the solution:
-msbuild SgmlReader.sln
+msbuild SgmlReader.sln /verbosity:minimal
+
+REM run .net core tests
+vstest.console d:\git\lovettchris\SgmlReader\SgmlTests\bin\Release\net5.0\SgmlTests.dll
+
+if "%MyKeyFile%" == "" goto :eof
+
+REM sign assemblies
+pwsh -f Common\full_sign.ps1
+
+REM create .nupkg
+nuget pack SgmlReader.nuspec
+
