989427207b363db4d658ef2084c336af3a5f3b32
clovett <lovettchris@hotmail.com>
2021-07-22 17:12:09 -0700

    publish 1.8.24

--- SgmlReaderUniversal/Properties/AssemblyInfo.cs
+++ SgmlReaderUniversal/Properties/AssemblyInfo.cs
@@ -23,6 +23,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.8.23")]
-[assembly: AssemblyFileVersion("1.8.23")]
+[assembly: AssemblyVersion("1.8.24")]
+[assembly: AssemblyFileVersion("1.8.24")]
 [assembly: ComVisible(false)]
